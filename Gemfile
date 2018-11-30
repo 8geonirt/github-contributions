9f7355a71881517d12f7c1750b74305bd513d580
José Trinidad Espinoza González <trinoeg8@gmail.com>
2018-11-30 16:19:45 -0600

    Remove devise - The project is going to require users to be authenticated by using Github, so devise is not required then.

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'jbuilder', '~> 2.5'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-gem 'devise'
 
 gem 'sidekiq'
 gem 'redis-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'home#index'
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'

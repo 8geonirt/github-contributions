e0da9cd4916ca398b840560c8f38e13038a8cdc3
José Trinidad Espinoza González <trinoeg8@gmail.com>
2019-07-04 11:33:32 -0500

    Contributions Counter Cache
    - Add counter cache so it will be easier to sort users by the total of
    contributions

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,7 +3,7 @@
 # Class that will perform CRUD operations to the Contributions table
 class Contribution < ApplicationRecord
   belongs_to :project
-  belongs_to :user
+  belongs_to :user, counter_cache: true
 
   validates :repository, :pull_request_url, presence: true
 end
